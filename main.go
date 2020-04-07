7d16b502355ed6c45cc292c42bc8621e586abe51
Tristan Lescut <tristan.lescut@bell.ca>
2020-04-06 20:17:40 -0400

    Add problem selection in main

--- main.go
+++ main.go
@@ -1,11 +1,26 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 
-	eulerproject "github.com/tlsrc/sandbox-go/euler-project"
+	euler "github.com/tlsrc/sandbox-go/euler-project"
 )
 
+type problem func() int
+
 func main() {
-	fmt.Println("Problem1: ", eulerproject.Problem0001())
+	var problems = make(map[int]problem)
+	problems[1] = euler.Problem0001
+	problems[2] = euler.Problem0002
+
+	var pb = flag.Int("pb", 1, "Please enter the -pb number")
+	flag.Parse()
+
+	if fn, ok := problems[*pb]; ok {
+		fmt.Printf("Problem %d : %d\n", *pb, fn())
+	} else {
+		fmt.Printf("Problem %d not solved yet\n", *pb)
+	}
+
 }
